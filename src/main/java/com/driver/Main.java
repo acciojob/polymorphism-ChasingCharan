9bffe848a9010d156b737ee2b60ecd30490e96e1
Charan Kumar Bijjaram <charankumarbijjaram@gmail.com>
2024-05-11 12:48:12 +0530

    the class runs successfully

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,37 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args) {
 
+        Product p = new Product();
+
+        int result1 = p.product(1,3);
+        System.out.println("Result of product(int x, int y): "+result1);
+
+        int result2 = p.product(1,2,3);
+        System.out.println("Result of product(int x, int y, int z): "+ result2);
+
+        double result3 = p.product(1.5,2.5);
+        System.out.println("Result of product(double x,double y): "+result3);
+
+    }
+
+
+    public static class Product{
+        int x;
+        int y;
+        int z;
+
+        public int product(int x, int y) {
+            return x*y;
+        }
+
+        public int product(int x, int y, int z) {
+            return x*y*z;
+        }
+
+        public double product(double x, double y) {
+            return x*y;
+        }
+    }
 }
